c9c120b159799f139f6dd016a3a4838311c8fa4f
DanielUPedersen1 <104761810+DanielUPedersen1@users.noreply.github.com>
2024-01-14 17:46:05 +0100

    Added Bootstrap v.5.3.2
    Added Bootstrap v.5.3.2

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <!-- META -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- TITLE -->
+        <title>Note Taker - University</title>
+
+        <!-- CSS Files -->
+        <link href="css/main_cascading_style_sheet.css" rel="stylesheet">
+        <link href="css/bootstrap-5_3_2-css-dist/bootstrap.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <h1>Hello, world!</h1>
+
+        <!-- JAVASCRIPT Files -->
+        <script src="js/main_javascript.js"></script>
+        <script src="js/bootstrap-5_3_2-js-dist/bootstrap.bundle.js"></script>
+
+    </body>
+</html>
